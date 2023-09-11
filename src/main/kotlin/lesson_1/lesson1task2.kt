259d1da543c7e9a6d84d2d8a2d320791f14e5730
epiccom <epic0330@gmail.com>
2023-09-11 18:57:33 +0300

    KS-1-2 выполнил 2-е задание из 1 урока

---
+++ src/main/kotlin/lesson_1/lesson1task2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main(){
+    val numberOfOrders: Int = 75
+    val notificationText: String = "Спасибо за покупку!"
+    println(numberOfOrders)
+    println(notificationText)
+
+    var workers: Int = 2000
+//    println(workers)
+    workers = 1999
+    println(workers)
+}
