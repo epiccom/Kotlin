01726d88476bc8c99f8a0875d56c198eaefe8aed
epiccom <epic0330@gmail.com>
2023-09-13 01:23:31 +0300

    KS-2-1 1-е задание из 2 урока

---
+++ src/main/kotlin/lesson_2/lesson2task1.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main() {
+    val student1 = 3.00
+    val student2 = 4.00
+    val student3 = 3.00
+    val student4 = 5.00
+
+    val ball = (student1+student2+student3+student4)/4.00
+
+    println("Средний балл: $ball")
+}
