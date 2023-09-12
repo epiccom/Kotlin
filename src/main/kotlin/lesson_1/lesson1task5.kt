985929e6852c35ba699da791d0c20c80a34f7f3a
epiccom <epic0330@gmail.com>
2023-09-12 18:50:22 +0300

    KS-1-5 исправил замечания, String.format использовать не получилось, не умею еще)

--- src/main/kotlin/lesson_1/lesson1task5.kt
+++ src/main/kotlin/lesson_1/lesson1task5.kt
@@ -1,11 +1,13 @@
 package lesson_1
 
-fun main(){
-    val seconds = 6480
-    val minutes = seconds/60 //108
-    val hours = minutes/60 //1.8
-    val minutes1 = minutes%60//0,8
-    val minutes2 = minutes1*60//48
-    val seconds1: String = "00"
-    println("0$hours:$minutes1:$seconds1")
+fun main() {
+
+    val totalSeconds = 6480
+
+    val minutes = totalSeconds / 60 //108
+    val hours = minutes / 60 //1
+    val minutes1 = minutes % 60 //48
+
+    //return String.format("%02d:%02d:00", hours, minutes1)
+    println("0$hours:$minutes1:00")
 }
