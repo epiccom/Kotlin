3dc72d69ed5166b17931ea54ace5510154ee9059
epiccom <epic0330@gmail.com>
2023-09-13 17:56:22 +0300

    KS-2-2 Сделал автоформатирование

--- src/main/kotlin/lesson_2/lesson2task2.kt
+++ src/main/kotlin/lesson_2/lesson2task2.kt
@@ -6,10 +6,10 @@ fun main() {
     val intern = 30
     val zpIntern = 20000
 
-    val totalZpRab = rab*zpRab
-    val totalZpIntern = intern*zpIntern
-    val totalZp = totalZpRab+totalZpIntern
-    val average = totalZp/(rab+intern)
+    val totalZpRab = rab * zpRab
+    val totalZpIntern = intern * zpIntern
+    val totalZp = totalZpRab + totalZpIntern
+    val average = totalZp / (rab + intern)
 
     println("Расходы на выплату постоянным работникам: $totalZpRab")
     println("Общие расходы после прихода стажеров: $totalZp")
