ac95d8454c4adcadd3b979492a3a84a64913132c
epiccom <epic0330@gmail.com>
2023-09-14 17:58:42 +0300

    KS-3-4 Исправил вывод сообщения под ТЗ

--- src/main/kotlin/lesson_1/lesson3-task4.kt
+++ src/main/kotlin/lesson_1/lesson3-task4.kt
@@ -5,11 +5,11 @@ fun main() {
     var moveFrom = "E2"
     var moveWhere = "E4"
 
-    println("$moveFrom-$moveWhere, $move")
+    println("$moveFrom-$moveWhere;$move")
 
     move = 2
     moveFrom = "D2"
     moveWhere = "D3"
 
-    println("$moveFrom-$moveWhere, $move")
+    println("$moveFrom-$moveWhere;$move")
 }
