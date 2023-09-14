79d68851a6588ec31e6d60328542c9880aa4f253
epiccom <epic0330@gmail.com>
2023-09-14 11:12:14 +0300

    KS-2-5 Исправил формулу, сделал автоформатирование, правильно использовал String.format()

--- src/main/kotlin/lesson_2/lesson2task5.kt
+++ src/main/kotlin/lesson_2/lesson2task5.kt
@@ -1,8 +1,12 @@
 package lesson_2
 
+import kotlin.math.pow
+
 fun main() {
     val vklad = 70000
-    val stavka = 16.7 // 16,7% годовых
+    val stavka = 16.7
 
-    println(vklad*(1 + stavka / 100) * 20) // 1633800.0
+    val result = vklad * (1 + stavka / 100).pow(20)
+
+    println(String.format("%.3f", result))
 }
