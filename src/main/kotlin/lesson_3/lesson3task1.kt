50ee15ad050d612543887d421193a8e96c1d7070
epiccom <epic0330@gmail.com>
2023-09-14 13:46:04 +0300

    KS-3-1 1-я задача из 3 урока

---
+++ src/main/kotlin/lesson_3/lesson3task1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val day = "день"
+    val night = "вечер"
+    val userName = "Иннокентий"
+
+    println("Добрый  $day, $userName!")
+    println("Добрый $night, $userName!")
+
+}
