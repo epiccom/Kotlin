103f2fc59bebfd8de2201c4050bfdb4933f57a88
epiccom <epic0330@gmail.com>
2023-09-15 15:29:20 +0300

    KS-4-2 Добавил константы, перенёс строки, сделал автоформатирование

--- src/main/kotlin/lesson_4/lesson4task2.kt
+++ src/main/kotlin/lesson_4/lesson4task2.kt
@@ -4,10 +4,19 @@ fun main() {
     var weight = 20
     var volume = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':${(weight >= 35 && weight <= 100) && (volume <= 100)}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':" +
+        "${(weight >= WEIGHT_MIN && weight <= WEIGHT_AND_VOLUME_MAX) && (volume <= WEIGHT_AND_VOLUME_MAX)}"
+    )
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':${(weight >= 35 && weight <= 100) && (volume <= 100)}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average':" +
+        "${(weight >= WEIGHT_MIN && weight <= WEIGHT_AND_VOLUME_MAX) && (volume <= WEIGHT_AND_VOLUME_MAX)}"
+    )
 }
+
+const val WEIGHT_MIN = 35
+const val WEIGHT_AND_VOLUME_MAX = 100
