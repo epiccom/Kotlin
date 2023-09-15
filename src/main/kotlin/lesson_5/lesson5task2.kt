93b635fae381dec02acc9dafa7f69dc0e45bb9ee
epiccom <epic0330@gmail.com>
2023-09-15 18:38:45 +0300

    KS-5-2 2-я задача из 5 урока

---
+++ src/main/kotlin/lesson_5/lesson5task2.kt
@@ -0,0 +1,19 @@
+package lesson_5
+
+import java.time.LocalDate
+
+fun main() {
+    println("Введите год рождения:")
+
+    val yearOfBirth = readln().toInt()
+    val year = LocalDate.now().year
+
+    if(year - yearOfBirth >= AGE_OF_MAJORITY) {
+        println("Показать экран со скрытым контентом")
+    }
+    if (year - yearOfBirth < AGE_OF_MAJORITY) {
+        println("Вернуться на главный экран")
+    }
+}
+
+const val AGE_OF_MAJORITY = 18
