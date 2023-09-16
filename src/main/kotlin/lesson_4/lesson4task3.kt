06543e6a68f772a90ca4b288d80d280f4e5cc212
epiccom <epic0330@gmail.com>
2023-09-16 13:44:15 +0300

    KS-4-3 добавил константы, укоротил строку

--- src/main/kotlin/lesson_4/lesson4task3.kt
+++ src/main/kotlin/lesson_4/lesson4task3.kt
@@ -1,10 +1,13 @@
 package lesson_4
 
 fun main() {
-    val weather = true
-    val tent = true
-    val wet = 20
-    val partOfYear = "зима"
-
-    println("Благоприятные ли условия сейчас для роста бобовых?${(weather == true) && (tent == true) && (wet == 20) && (partOfYear != "зима")}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых?" +
+        "${(WEATHER == true) && (TENT == true) && (WET == 20) && (PART_OF_YEAR != "зима")}"
+    )
 }
+
+const val WEATHER = true
+const val TENT = true
+const val WET = 20
+const val PART_OF_YEAR = "зима"
