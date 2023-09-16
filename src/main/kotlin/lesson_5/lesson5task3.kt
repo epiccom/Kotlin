5af376b6f2d577805a2d389fed7986b3b21dea9d
epiccom <epic0330@gmail.com>
2023-09-16 15:49:07 +0300

    KS-5-3 3-я задача из 5 урока

---
+++ src/main/kotlin/lesson_5/lesson5task3.kt
@@ -0,0 +1,22 @@
+package lesson_5
+
+fun main() {
+
+    println("Введите два числа от 1 до 100 через запятую, например : 10,45")
+
+    val input = readln()
+    val result = input.split(",")
+    val fsInput = result[0].toInt()
+    val scInput = result[1].toInt()
+
+    if (fsInput == FIRST_NUM && scInput == SECOND_NUM)
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if (fsInput == FIRST_NUM || scInput == SECOND_NUM)
+        println("Вы выиграли утешительный приз!")
+        else println("Неудача! Крутите барабан!")
+
+    println("Для победы нужны были числа: $FIRST_NUM и $SECOND_NUM")
+}
+
+const val FIRST_NUM = 6
+const val SECOND_NUM = 16
