c68a2fc5e26ca9600a9210a87f0531fdb7b70f2e
epiccom <epic0330@gmail.com>
2023-09-16 14:40:12 +0300

    KS-4-5 Избавился от магических чисел, убрал лишнюю переменную, использовал переменные типа Boolean

--- src/main/kotlin/lesson_4/lesson4task5.kt
+++ src/main/kotlin/lesson_4/lesson4task5.kt
@@ -3,23 +3,25 @@ package lesson_4
 fun main() {
     println(
         """
-    Введите данные в формате:[Повреждения,Экипаж,Провизия,Погода]
-    Пример вводных данных: [true,50,45,false]
+    Введите данные в формате:[Повреждения(да/нет),Экипаж,Провизия,Погода благоприятна(да/нет)]
+    Пример вводных данных: [да,50,45,нет]
     """.trimIndent()
     )
     val input = readln()
-    val sym = ","
-    val result = input.split(sym)
-    val damage: String = result[0]
+    val result = input.split(",")
+    val isDamaged = (result[0] == "нет")
     val team = result[1].toInt()
     val food = result[2].toInt()
-    val weather: String = result[3]
+    val weather = (result[3] == "да")
 
-    if (damage == "false" && team in 55..70 && food >= 50 && weather == "true")
+    if (isDamaged == true && team in TEAM_MIN..TEAM_MAX && food >= FOOD_MIN && weather == true)
         println("Корабль может отправляться в плавание!")
-    else if (team >= 70 && food >= 50 && weather == "true")
+    else if (team >= TEAM_MAX && food >= FOOD_MIN && weather == true)
         println("Повреждения не значительны, корабль может отправляться в плавание!")
     else println("Повреждения критические, корабль не может отправляться в плавание!")
 
-
 }
+
+const val TEAM_MIN = 55
+const val TEAM_MAX = 70
+const val FOOD_MIN = 50
