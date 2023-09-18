a50487804ce74e0e4d0a3bb97755c1b76242babd
epiccom <epic0330@gmail.com>
2023-09-18 10:33:56 +0300

    KS-4-5 использовал .toBoolean

--- src/main/kotlin/lesson_4/lesson4task5.kt
+++ src/main/kotlin/lesson_4/lesson4task5.kt
@@ -3,18 +3,18 @@ package lesson_4
 fun main() {
     println(
         """
-    Введите данные в формате:[Повреждения(да/нет),Экипаж,Провизия,Погода благоприятна(да/нет)]
-    Пример вводных данных: [да,50,45,нет]
+    Введите данные в формате:[Повреждения(true/false),Экипаж,Провизия,Погода благоприятна(true/false)]
+    Пример вводных данных: [true,50,45,false]
     """.trimIndent()
     )
     val input = readln()
     val result = input.split(",")
-    val isDamaged = (result[0] == "нет")
+    val isDamaged = result[0].toBoolean()
     val team = result[1].toInt()
     val food = result[2].toInt()
-    val weather = (result[3] == "да")
+    val weather = result[3].toBoolean()
 
-    if (isDamaged == true && team in TEAM_MIN..TEAM_MAX && food >= FOOD_MIN && weather == true)
+    if (isDamaged == false && team in TEAM_MIN..TEAM_MAX && food >= FOOD_MIN && weather == true)
         println("Корабль может отправляться в плавание!")
     else if (team >= TEAM_MAX && food >= FOOD_MIN && weather == true)
         println("Повреждения не значительны, корабль может отправляться в плавание!")
