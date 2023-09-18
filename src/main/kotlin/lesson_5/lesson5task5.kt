fb278744ed95072605229037a23e39fe9a04a7c3
epiccom <epic0330@gmail.com>
2023-09-18 11:19:04 +0300

    KS-5-5 5-я задача из 5 урока

---
+++ src/main/kotlin/lesson_5/lesson5task5.kt
@@ -0,0 +1,22 @@
+package lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+    val firstNum = Random.nextInt(1,100)
+    val secondNum = Random.nextInt(1,100)
+    println("Введите два числа от 1 до 100 через запятую, например : 10,45")
+
+    val input = readln()
+    val result = input.split(",")
+    val fsInput = result[0].toInt()
+    val scInput = result[1].toInt()
+
+    if (fsInput == firstNum && scInput == secondNum)
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if (fsInput == firstNum || scInput == secondNum)
+        println("Вы выиграли утешительный приз!")
+    else println("Неудача! Крутите барабан!")
+
+    println("Для победы нужны были числа: $firstNum и $secondNum")
+}
