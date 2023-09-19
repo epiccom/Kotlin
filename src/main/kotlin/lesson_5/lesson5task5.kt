e9e3ae2fe8560315aec4fd0ff43790480ae2f1f6
epiccom <epic0330@gmail.com>
2023-09-19 10:38:39 +0300

    KS-5-5 Добавил проверку в обратном порядке

--- src/main/kotlin/lesson_5/lesson5task5.kt
+++ src/main/kotlin/lesson_5/lesson5task5.kt
@@ -3,8 +3,8 @@ package lesson_5
 import kotlin.random.Random
 
 fun main() {
-    val firstNum = Random.nextInt(1,100)
-    val secondNum = Random.nextInt(1,100)
+    val firstNum = Random.nextInt(1, 100)
+    val secondNum = Random.nextInt(1, 100)
     println("Введите два числа от 1 до 100 через запятую, например : 10,45")
 
     val input = readln()
@@ -12,9 +12,9 @@ fun main() {
     val fsInput = result[0].toInt()
     val scInput = result[1].toInt()
 
-    if (fsInput == firstNum && scInput == secondNum)
+    if ((fsInput == firstNum && scInput == secondNum) || (fsInput == secondNum && scInput == firstNum))
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (fsInput == firstNum || scInput == secondNum)
+    else if (fsInput == firstNum || scInput == secondNum || fsInput == secondNum || scInput == firstNum)
         println("Вы выиграли утешительный приз!")
     else println("Неудача! Крутите барабан!")
 
