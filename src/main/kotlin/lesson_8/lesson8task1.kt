35d2eb388cd564c009524268e38300620fd86e38
epiccom <epic0330@gmail.com>
2023-09-27 10:31:21 +0300

    KS-8-1 1 задача из 8 урока

---
+++ src/main/kotlin/lesson_8/lesson8task1.kt
@@ -0,0 +1,7 @@
+package lesson_8
+
+fun main() {
+    val views = arrayOf(10156, 11216, 13578, 9872, 10003, 12876, 6520)
+    println("Просмотров за неделю: ${views[0] + views[1] + views[3] + views[4] + views[5] + views[6]}")
+
+}
