1829eeb35f5dd0a742af7349ff61938113efa0e7
epiccom <epic0330@gmail.com>
2023-09-29 13:00:50 +0300

    KS-9-4 засунул всё в цикл)

--- src/main/kotlin/lesson_9/lesson9task4.kt
+++ src/main/kotlin/lesson_9/lesson9task4.kt
@@ -2,15 +2,15 @@ package lesson_9
 
 fun main() {
     println("Введите 5 названий ингридиентов блюда через запятую:")
-
-    val arrayIngridients = readln().split(",")
-    if (arrayIngridients.size == 5) {
-        arrayIngridients.toTypedArray().sortedArray().forEach {
-            println(it)
+    while (true) {
+        val arrayIngridients = readln().split(",").toTypedArray().sortedArray()
+        if (arrayIngridients.size == 5) {
+            arrayIngridients.forEach {
+                println(it)
+            }
+            return
+        } else {
+            println("Элементов должно быть 5")
         }
-    } else {
-        println("Элементов должно быть 5")
-        return main()
     }
-
 }
