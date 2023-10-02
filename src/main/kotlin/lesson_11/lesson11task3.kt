510e54d48f5cebf89e4b2444299bca30faed8e5c
epiccom <epic0330@gmail.com>
2023-10-02 11:57:08 +0300

    KS-11-3 3 задача из 11 урока

--- src/main/kotlin/lesson_11/lesson11task3.kt
+++ src/main/kotlin/lesson_11/lesson11task3.kt
@@ -1,27 +1,32 @@
 package lesson_11
 
 class Room(
+    val cover: String,
     val name: String,
-    val users: List<User>,
+    val members: List<User>,
 )
 
 class User(
+    val avatar: String,
     val name: String,
     val status: String,
 )
 
 fun main() {
     val firstUser = User(
-        name = "First User",
-        status = "mute",
+        avatar = "Аватар 1",
+        name = "Имя 1",
+        status = "разговаривает",
     )
     val secondUser = User(
-        name = "Second User",
-        status = "speak",
+        avatar = "Аватар 1",
+        name = "Имя 2",
+        status = "микрофон выключен",
     )
-    val room1 = Room(
-        name = "Room",
-        users = listOf(firstUser, secondUser),
+    val firstRoom = Room(
+        cover = "Обложка 1",
+        name = "Комната 1",
+        members = listOf(firstUser, secondUser)
     )
-    println("Пользователи комнаты <${room1.name}>: ${room1.users}")
+
 }
