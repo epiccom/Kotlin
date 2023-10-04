c7dd8ff8f4cb1fe0fc65b24626254483154d5a65
epiccom <epic0330@gmail.com>
2023-10-04 12:29:34 +0300

    KS-12-1 забыл переменную дождя, добавил

--- src/main/kotlin/lesson_12/lesson12task1.kt
+++ src/main/kotlin/lesson_12/lesson12task1.kt
@@ -3,6 +3,7 @@ package lesson_12
 class InfoOfWeather() {
     var dayTemperature = 35
     var nightTemperature = 15
+    var rain = true
     var atmospherePressure = 764
 
 }
@@ -10,14 +11,16 @@ fun main() {
     val object1 = InfoOfWeather()
     object1.dayTemperature = 40
     object1.nightTemperature = 17
+    object1.rain = false
     object1.atmospherePressure = 764
     println("Температура днём: ${object1.dayTemperature}°, температура ночью: ${object1.nightTemperature}°," +
-            " атмосферное давление: ${object1.atmospherePressure} мм рт. ст.")
+            "был ли дождь: ${object1.rain}, атмосферное давление: ${object1.atmospherePressure} мм рт. ст.")
 
     val object2 = InfoOfWeather()
     object2.dayTemperature = 10
     object2.nightTemperature = -10
+    object2.rain = true
     object2.atmospherePressure = 724
     println("Температура днём: ${object2.dayTemperature}°, температура ночью: ${object2.nightTemperature}°," +
-            " атмосферное давление: ${object2.atmospherePressure} мм рт. ст.")
+            "был ли дождь: ${object2.rain}, атмосферное давление: ${object2.atmospherePressure} мм рт. ст.")
 }
