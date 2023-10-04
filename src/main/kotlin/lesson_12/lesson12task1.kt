b6e03576cf42730fbe86e0d8560d8a3434c9afaa
epiccom <epic0330@gmail.com>
2023-10-04 10:15:21 +0300

    KS-12-1 инициализация только конкретными значениями

--- src/main/kotlin/lesson_12/lesson12task1.kt
+++ src/main/kotlin/lesson_12/lesson12task1.kt
@@ -1,21 +1,23 @@
 package lesson_12
 
 class InfoOfWeather() {
-    var dayTemperature = "35°"
-    var nightTemperature = "15°"
-    var atmospherePressure = "764 мм рт.ст."
+    var dayTemperature = 35
+    var nightTemperature = 15
+    var atmospherePressure = 764
 
 }
 fun main() {
     val object1 = InfoOfWeather()
-    object1.dayTemperature = "Температура днём 40°"
-    object1.nightTemperature = "Температура ночью 17°"
-    object1.atmospherePressure = "Атмосферное давление 764 мм рт. ст."
-    println("${object1.dayTemperature}, ${object1.nightTemperature}, ${object1.atmospherePressure}")
+    object1.dayTemperature = 40
+    object1.nightTemperature = 17
+    object1.atmospherePressure = 764
+    println("Температура днём: ${object1.dayTemperature}°, температура ночью: ${object1.nightTemperature}°," +
+            " атмосферное давление: ${object1.atmospherePressure} мм рт. ст.")
 
     val object2 = InfoOfWeather()
-    object2.dayTemperature = "Температура днём 10°"
-    object2.nightTemperature = "Температура ночью -10°"
-    object2.atmospherePressure = "Атмосферное давление 724 мм рт. ст."
-    println("${object2.dayTemperature}, ${object2.nightTemperature}, ${object2.atmospherePressure}")
+    object2.dayTemperature = 10
+    object2.nightTemperature = -10
+    object2.atmospherePressure = 724
+    println("Температура днём: ${object2.dayTemperature}°, температура ночью: ${object2.nightTemperature}°," +
+            " атмосферное давление: ${object2.atmospherePressure} мм рт. ст.")
 }
