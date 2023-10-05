a8594751119b85e410290130688da9fbeb76e97c
epiccom <epic0330@gmail.com>
2023-10-05 11:54:14 +0300

    KS-11-5 переделал реализацию списка

--- src/main/kotlin/lesson_11/lesson11task5.kt
+++ src/main/kotlin/lesson_11/lesson11task5.kt
@@ -1,8 +1,8 @@
 package lesson_11
 
 class Forum(
-    var members: List<User0>,
-    var messages: List<Message>,
+    val members: MutableList<User0> = mutableListOf(),
+    val messages: MutableList<Message> = mutableListOf(),
 ) {
     fun createNewUser(
         userId: Int,
@@ -10,7 +10,7 @@ class Forum(
         userPassword: String,
         userEmail: String,
     ) {
-        members = listOf(User0(userId, userLogin, userPassword, userEmail))
+        members.add(User0(userId, userLogin, userPassword, userEmail))
     }
 
     fun createNewMessage(
@@ -18,11 +18,12 @@ class Forum(
         author: String,
         textOfMessage: String,
     ) {
-        messages = listOf(Message(authorId, author, textOfMessage))
+        messages.add(Message(authorId, author, textOfMessage))
     }
 
     fun printThread() {
-        println(messages)
+        println(messages[0])
+        println(messages[1])
     }
 }
 
@@ -33,7 +34,7 @@ class Message(
 )
 
 fun main() {
-    val forum = Forum(listOf(), listOf())
+    val forum = Forum()
     forum.createNewUser(0, "user 1", "1234", "mail1@mail.com")
     forum.createNewUser(1, "user 2", "4321", "mail2@mail.com")
 
