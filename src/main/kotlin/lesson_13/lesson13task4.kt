d7e807c5a2c74c09e74f19c0ab16efaa3fe8729e
epiccom <epic0330@gmail.com>
2023-10-08 15:00:30 +0300

    KS-13-4 добавил проверку на isEmpty, добавил игнор регистра

--- src/main/kotlin/lesson_13/lesson13task4.kt
+++ src/main/kotlin/lesson_13/lesson13task4.kt
@@ -1,19 +1,25 @@
 package lesson_13
 
+import java.util.*
+
 class Contact2(val name: String?, val number: String?, val company: String?)
 
 val contacts: MutableList<Contact2> = mutableListOf()
 fun addContact() {
     println("Введите имя:")
     var userName: String? = readln()
-    if (userName == "") userName = null
+    if (userName != null) {
+        if (userName.isEmpty()) userName = null
+    }
 
     println("Введите номер:")
     val userNumber = readln()
 
     println("Введите компанию:")
     var userCompany: String? = readln()
-    if (userCompany == "") userCompany = null
+    if (userCompany != null) {
+        if (userCompany.isEmpty()) userCompany = null
+    }
 
     if (userNumber == "") {
         println("Поле с номером не может быть пустым, повторите ввод")
@@ -22,7 +28,7 @@ fun addContact() {
         contacts.add(Contact2(userName, userNumber, userCompany))
         println("Контакт добавлен")
         println("Если хотите добавить новую запись, введите \"да\"")
-        val answer = readln()
+        val answer = readln().lowercase(Locale.getDefault())
         if (answer == "да") return addContact() else return
     }
 }
