74644b44c79cd9c891e1db6ef9180817a7016dcb
epiccom <epic0330@gmail.com>
2023-10-08 13:47:10 +0300

    KS-13-4 4 задача из 13 урока

---
+++ src/main/kotlin/lesson_13/lesson13task4.kt
@@ -0,0 +1,35 @@
+package lesson_13
+
+class Contact2(val name: String?, val number: String?, val company: String?)
+
+val contacts: MutableList<Contact2> = mutableListOf()
+fun addContact() {
+    println("Введите имя:")
+    var userName: String? = readln()
+    if (userName == "") userName = null
+
+    println("Введите номер:")
+    val userNumber = readln()
+
+    println("Введите компанию:")
+    var userCompany: String? = readln()
+    if (userCompany == "") userCompany = null
+
+    if (userNumber == "") {
+        println("Поле с номером не может быть пустым, повторите ввод")
+        return addContact()
+    } else {
+        contacts.add(Contact2(userName, userNumber, userCompany))
+        println("Контакт добавлен")
+        println("Если хотите добавить новую запись, введите \"да\"")
+        val answer = readln()
+        if (answer == "да") return addContact() else return
+    }
+}
+
+fun main() {
+    addContact()
+    contacts.forEach {
+        println("Имя: ${it.name}\nНомер: ${it.number}\nКомпания: ${it.company}")
+    }
+}
