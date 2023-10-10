65ce18d6d016b2c5803e81b17bd741f5f4322732
epiccom <epic0330@gmail.com>
2023-10-10 19:58:36 +0300

    KS-14-1 увидел 2 задачу и понял что перестарался, исправил

--- src/main/kotlin/lesson_14/lesson14task1.kt
+++ src/main/kotlin/lesson_14/lesson14task1.kt
@@ -4,7 +4,6 @@ class StandardShip(
     val name: String,
     val speed: Byte,
     val carrying: Byte,
-    val crushIce: Boolean = false,
 ) {
     fun shipSpeed() {
         println("$name движется со скоростью $speed узлов")
@@ -14,30 +13,11 @@ class StandardShip(
         println("$name перевозит $carrying ящиков груза")
     }
 
-    fun abilityToCrushIce() {
-        if (crushIce) println("$name может проходить через льды")
-        else println("$name не может проходить через льды")
-    }
 }
 
 fun main() {
     val standardShip = StandardShip("Стандартный корабль", 20, 15)
-    val cargoShip = StandardShip("Грузовой корабль", 12, 30)
-    val iceBreaker = StandardShip("Ледокол", 6, 6, true)
 
     standardShip.shipSpeed()
     standardShip.amountOfCargoBoxes()
-    standardShip.abilityToCrushIce()
-    println()
-
-    cargoShip.shipSpeed()
-    cargoShip.amountOfCargoBoxes()
-    cargoShip.abilityToCrushIce()
-    println()
-
-    iceBreaker.shipSpeed()
-    iceBreaker.amountOfCargoBoxes()
-    iceBreaker.abilityToCrushIce()
-
-
 }
