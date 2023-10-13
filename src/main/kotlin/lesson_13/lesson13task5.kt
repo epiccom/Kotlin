2911d8a09dda0b8fe055bff1073a8c3d55597a6e
epiccom <144666837+epiccom@users.noreply.github.com>
2023-10-13 10:53:20 +0300

    KS-14-5 5 задача из 14 урока

--- src/main/kotlin/lesson_13/lesson13task5.kt
+++ src/main/kotlin/lesson_13/lesson13task5.kt
@@ -4,8 +4,8 @@ fun main() {
     try {
         println("Введите номер:")
         val number: Long
-        val strNumber = readLine().toString()
-        number = strNumber.toLong()
+        val strNumber = readlnOrNull()
+        number = strNumber!!.toLong()
     } catch (e: Exception) {
         println("Номер может содержать только цифры")
     }
