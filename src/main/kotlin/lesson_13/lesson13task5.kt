b57384782ecd95b41ced79f30c98c0e49164a084
epiccom <144666837+epiccom@users.noreply.github.com>
2023-10-14 10:32:29 +0300

    KS-13-5 использовал ?. вместо !!

--- src/main/kotlin/lesson_13/lesson13task5.kt
+++ src/main/kotlin/lesson_13/lesson13task5.kt
@@ -3,9 +3,9 @@ package lesson_13
 fun main() {
     try {
         println("Введите номер:")
-        val number: Long
+        val number: Long?
         val strNumber = readlnOrNull()
-        number = strNumber!!.toLong()
+        number = strNumber?.toLong()
     } catch (e: Exception) {
         println("Номер может содержать только цифры")
     }
