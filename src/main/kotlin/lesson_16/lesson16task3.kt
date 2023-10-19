06eced49cc527fac6cb10c3e2a4565e98acd5916
epiccom <144666837+epiccom@users.noreply.github.com>
2023-10-19 15:16:37 +0500

    KS-16-3 метод принимает пароль для проверки, не понял на счёт true/false как сделать

--- src/main/kotlin/lesson_16/lesson16task3.kt
+++ src/main/kotlin/lesson_16/lesson16task3.kt
@@ -1,14 +1,13 @@
 package lesson_16
 
-class Profile0(val login: String, private val password: Int) {
-    private val userPassword = 1234
-    fun checkPassword() {
+class Profile0(private val userPassword: Int) {
+    fun checkPassword(password: Int) {
         if (password == userPassword) println("Пароль введен верно")
         else println("Пароль введен не верно")
     }
 }
 
 fun main() {
-    val user = Profile0("Login",12345)
-    user.checkPassword()
+    val user = Profile0(12345)
+    user.checkPassword(1234)
 }
