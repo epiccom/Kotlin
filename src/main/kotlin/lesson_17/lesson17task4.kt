ab78399818887a60122f9d36148a356508f91bc3
epiccom <144666837+epiccom@users.noreply.github.com>
2023-10-26 17:29:22 +0500

    KS-17-4 перенёс поля в основной конструктор

--- src/main/kotlin/lesson_17/lesson17task4.kt
+++ src/main/kotlin/lesson_17/lesson17task4.kt
@@ -1,17 +1,16 @@
 package lesson_17
 
-class Package0() {
-    val number = 0
-    var location = "base"
+class Package0(val number: Int, location: String,) {
+    var location = location
         set(value) {
             field = value
             movements += 1
         }
     var movements = 0
 }
+
 fun main() {
-    val pack0 = Package0()
-    println(pack0.location)
+    val pack0 = Package0(1,"base")
     println(pack0.movements)
     pack0.location = "in transit"
     println(pack0.movements)
